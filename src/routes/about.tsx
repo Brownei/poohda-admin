259efed3ee38e575ef674086121c6ca1be8993e6
Michael Omonedo <rukkyomonedo@gmail.com>
2024-12-14 22:13:56 +0100

    Styling changes to the "Add Clothes" component

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,14 +1,16 @@
-import * as React from 'react'
-import { createFileRoute } from '@tanstack/react-router'
+import * as React from "react";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/about')({
+export const Route = createFileRoute("/about")({
   component: AboutComponent,
-})
+});
 
 function AboutComponent() {
   return (
     <div className="p-2 mt-5 w-full mr-6">
-      <h3>About</h3>
+      <div className="p-2 mt-5 w-full text-center">
+        <h3 className="text-2xl text-green-300">About</h3>
+      </div>
     </div>
-  )
+  );
 }
--- src/routes/waitlist.tsx
+++ src/routes/waitlist.tsx
@@ -1,9 +1,16 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/waitlist')({
+export const Route = createFileRoute("/waitlist")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div className="p-2 mt-5 w-full mr-6">Waitlist</div>
+  return (
+    <div className="p-2 mt-5 w-full mr-6">
+      {" "}
+      <div className="p-2 mt-5 w-full text-center">
+        <h3 className="text-2xl text-green-300">Waitlist</h3>
+      </div>
+    </div>
+  );
 }
